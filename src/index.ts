c061580837b47e97b2cc83ff8174ed613098e8f4
Dale Bradley <dbradley@companieshouse.gov.uk>
2021-02-17 17:03:06 +0000

    Update logic for use with gds component

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { checkCookieIsSet } from './utilities'
 
 if (checkCookieIsSet() === 'yes') {
   const cookieBanner = document.getElementById('cookie-banner')
-  if (cookieBanner?.style.display === 'none') {
-    cookieBanner.style.display = 'block'
-  } else if (cookieBanner != null) {
-    cookieBanner.style.display = 'none'
+  if (cookieBanner !== null) {
+    cookieBanner.hidden = true
   }
 }
 
 export { acceptCookies } from './utilities'
 export { rejectCookies } from './utilities'
+export { hideBannerAlert } from './utilities'
