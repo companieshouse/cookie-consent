ef1541083835a6f6e5ee4b97e431316e5d6e0168
mikecsmith <msmith1@companieshouse.gov.uk>
2021-02-22 20:57:31 +0000

    polyfill String.includes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ export { acceptCookies } from './utilities'
 export { rejectCookies } from './utilities'
 export { hideBannerAlert } from './utilities'
 export { start } from './utilities'
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search as any instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    }
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
