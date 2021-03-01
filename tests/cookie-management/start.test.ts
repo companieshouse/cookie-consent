ca6209e3693ff213eed7b04286c2e43621c6f2f9
mikecsmith <msmith1@companieshouse.gov.uk>
2021-03-01 09:22:13 +0000

    Add error state checking to start tests

--- tests/cookie-management/start.test.ts
+++ tests/cookie-management/start.test.ts
@@ -1,6 +1,6 @@
 import { expect, use } from 'chai'
 import { CookieJar } from 'jsdom'
-import { spy } from 'sinon'
+import { spy, stub } from 'sinon'
 import { start } from '../../src/cookie-management'
 import { createJSDOM, defaultHTML, defaultURL } from '../test-utilities'
 import { CHCookie } from '../../src/types'
@@ -35,7 +35,7 @@ describe('Start function tests', () => {
     expect(mockCookiesRejectedMessage).to.have.attribute('hidden')
   })
 
-  it('should call the callback if the user has allowed cookies', () => {
+  it('should call the callback if the user has allowed cookies but not show any dom elements', () => {
     const cookie: CHCookie = {
       userHasAllowedCookies: 'yes',
       cookiesAllowed: COOKIES
@@ -87,6 +87,38 @@ describe('Start function tests', () => {
     start(callback)
 
     expect(callback).to.not.have.been.called()
+    expect(mockAcceptOrRejectMessage).to.have.attribute('hidden')
+    expect(mockCookieBanner).to.have.attribute('hidden')
+    expect(mockCookiesAcceptedMessage).to.have.attribute('hidden')
+    expect(mockCookiesRejectedMessage).to.have.attribute('hidden')
+  })
+
+  it('should log an error if the callback throws and not show any dom elements', () => {
+    const cookie: CHCookie = {
+      userHasAllowedCookies: 'yes',
+      cookiesAllowed: COOKIES
+    }
+    const cookieJar = new CookieJar()
+    cookieJar.setCookie(`${COOKIE_NAME}=${JSON.stringify(cookie)}`, defaultURL, (x) => console.log)
+
+    createJSDOM(defaultHTML, {
+      url: defaultURL,
+      cookieJar
+    })
+
+    const mockAcceptOrRejectMessage = document.getElementById('accept-or-reject-message')
+    const mockCookieBanner = document.getElementById('cookie-banner')
+    const mockCookiesAcceptedMessage = document.getElementById('accepted-cookies-message')
+    const mockCookiesRejectedMessage = document.getElementById('rejected-cookies-message')
+
+    const consoleSpy = spy()
+    const callback = stub().throws()
+    global.window.console.error = consoleSpy
+
+    start(callback)
+
+    expect(callback).to.have.been.called()
+    expect(consoleSpy).to.have.been.called()
     expect(mockAcceptOrRejectMessage).to.have.attribute('hidden')
     expect(mockCookieBanner).to.have.attribute('hidden')
     expect(mockCookiesAcceptedMessage).to.have.attribute('hidden')
