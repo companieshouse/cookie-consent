72bbed6c220b046ce559feb264b1a76c83a5483c
mikecsmith <msmith1@companieshouse.gov.uk>
2021-03-01 08:29:51 +0000

    Add tests for rejectCookies method

---
+++ tests/cookie-management/rejectCookies.test.ts
@@ -0,0 +1,83 @@
+import { spy, stub } from 'sinon'
+import { use, expect } from 'chai'
+import { createJSDOM } from '../test-utilities'
+import { rejectCookies } from '../../src'
+import { COOKIE_NAME } from '../../src/constants'
+import dirtyChai = require('dirty-chai')
+import sinonChai = require('sinon-chai')
+import chaiDom = require ('chai-dom')
+
+use(dirtyChai)
+use(sinonChai)
+use(chaiDom)
+
+const cleanup = createJSDOM()
+
+describe('Reject Cookies tests', () => {
+  afterEach(cleanup)
+
+  it('should create a cookie and unhide the cookies rejected message', () => {
+    const html = `
+    <html lang="en">
+      <body>
+        <div id="cookie-banner">
+          <div id="accept-or-reject-message"></div>
+          <div id="accepted-cookies-message" hidden></div>
+          <div id="rejected-cookies-message" hidden></div>
+        </div>
+      </body>
+    </html>`
+    createJSDOM(html)
+    const callback = spy()
+
+    rejectCookies(callback)
+
+    const mockAcceptOrRejectMessage = document.getElementById('accept-or-reject-message')
+    const mockCookiesRejectedMessage = document.getElementById('rejected-cookies-message')
+
+    expect(document.cookie).to.equal(`${COOKIE_NAME}={"userHasAllowedCookies":"no","cookiesAllowed":[]}`)
+    expect(mockAcceptOrRejectMessage).to.have.attribute('hidden')
+    expect(mockCookiesRejectedMessage).to.not.have.attribute('hidden')
+  })
+
+  it('should not error and still set a cookie if the dom is improperly formatted', () => {
+    const html = `
+    <html lang="en">
+      <body>
+      </body>
+    </html>`
+    createJSDOM(html)
+    const callback = spy()
+
+    rejectCookies(callback)
+
+    expect(document.cookie).to.equal(`${COOKIE_NAME}={"userHasAllowedCookies":"no","cookiesAllowed":[]}`)
+  })
+
+  it('should set a cookie, hide the accept or reject message, show the rejected message and log an error if the callback throws', () => {
+    const html = `
+    <html lang="en">
+      <body>
+        <div id="cookie-banner">
+          <div id="accept-or-reject-message"></div>
+          <div id="accepted-cookies-message" hidden></div>
+          <div id="rejected-cookies-message" hidden></div>
+        </div>
+      </body>
+    </html>`
+    createJSDOM(html)
+
+    const consoleSpy = spy()
+    const badCallback = stub().throws()
+    window.console.error = consoleSpy
+    rejectCookies(badCallback)
+
+    const mockAcceptOrRejectMessage = document.getElementById('accept-or-reject-message')
+    const mockCookiesRejectedMessage = document.getElementById('rejected-cookies-message')
+
+    expect(document.cookie).to.equal(`${COOKIE_NAME}={"userHasAllowedCookies":"no","cookiesAllowed":[]}`)
+    expect(consoleSpy).to.have.been.called()
+    expect(mockAcceptOrRejectMessage).to.have.attribute('hidden')
+    expect(mockCookiesRejectedMessage).to.not.have.attribute('hidden')
+  })
+})
